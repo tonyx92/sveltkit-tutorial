cd3ff745ee971c817039bf63b2b4fc800c428880
tonyx92 <tognolimartino@gmail.com>
2023-09-07 16:47:40 +0200

    aggiunto layout routing

---
+++ 3-routing/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    import '../routes/app.css'
+</script>
+
+<nav>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/posts">Posts</a>
+
+        </li>
+    </ul>
+</nav>
+
+<slot />
