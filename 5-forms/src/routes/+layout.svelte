ba658e9b363ffd6da6b751cafa97ba3368bd0d11
tonyx92 <tognolimartino@gmail.com>
2023-09-09 23:04:53 +0200

    iniziamo form sveltekit e utilizziamo un server-only module che contiene tutte le info e protegge anche quelle sensibili (API keys, ecc)

---
+++ 5-forms/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<slot />
+
+<style>
+  @import 'https://unpkg.com/@picocss/pico@latest/css/pico.min.css';
+
+  :global(body) {
+    padding: 2rem;
+  }
+
+  :global(input, button) {
+    border-radius: 1rem;
+  }
+</style>
