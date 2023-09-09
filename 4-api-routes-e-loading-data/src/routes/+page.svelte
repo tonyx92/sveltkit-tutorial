d9bb0bb80101598b320a061142915b5791551f26
tonyx92 <tognolimartino@gmail.com>
2023-09-09 18:15:59 +0200

    aggiunto get database per iniziare ad estrarre info posts utilizzando  db tramite prisma client, avuto diversi problemi ricordarsi come funziona  in svelte e fare attenzione handlerequest che in posts/+server.ts bloccava l'invio dei dati

--- 4-api-routes-e-loading-data/src/routes/+page.svelte
+++ 4-api-routes-e-loading-data/src/routes/+page.svelte
@@ -1,27 +1,18 @@
 <script lang="ts">
-    import type { Post } from '@prisma/client'
-  
-    async function getPosts() {
-      const response = await fetch('/api/posts')
-      const posts: Post[] = await response.json()
-      return posts
-    }
-  </script>
-  
-  <h1>Posts</h1>
-  
-  {#await getPosts()}
-    <p>Loading...</p>
-  {:then posts}
-    <p>Showing {posts.length} posts.</p>
-  
-    <ul>
-      {#each posts as post}
-        <li>
-          <a href="/posts/{post.title}">{post.title}</a>
-        </li>
-      {/each}
-    </ul>
-  {:catch error}
-    <p>{error.message}</p>
-  {/await}
+  async function subscribe(event: Event) {
+    const form = event.target as HTMLFormElement
+    const data = new FormData(form)
+
+    await fetch('/api/newsletter', {
+      method: 'POST',
+      body: data
+    })
+  }
+</script>
+
+<h1>Newsletter</h1>
+
+<form on:submit|preventDefault={subscribe}>
+  <input type="email" name="email" />
+  <button>Subscribe</button>
+</form>
--- 4-api-routes-e-loading-data/src/routes/api/posts/+server.ts
+++ 4-api-routes-e-loading-data/src/routes/api/posts/+server.ts
@@ -1,7 +1,18 @@
 import { json } from '@sveltejs/kit'
-import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async () => {
-  const posts = await db.post.findMany()
-  return json(posts);
-};
+import db from '$lib/database'
+
+
+export async function GET(event) {
+  const posts = await db.post.findMany({
+    // get random numbers of posts to test caching
+    take: Math.round(Math.random() * 30)
+  })
+
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching
+  event.setHeaders({
+    'Cache-Control': 'max-age=60'
+  })
+
+  return json(posts)
+}
